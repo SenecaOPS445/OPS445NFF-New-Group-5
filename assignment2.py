f87802c334288c20a6641ed402973c5c0811c1ef
Liam Waterman <lwaterman1@myseneca.ca>
2025-04-03 20:48:29 -0400

    lwaterman a2

--- assignment2.py
+++ assignment2.py
@@ -1,5 +1,4 @@
-def get_clients(clients_arg):
-    if clients_arg.lower() == 'all':
-        return CLIENT_IPS
-    return [ip.strip() for ip in
-clients_arg.split(',')]
+def get_clients(clients_arg): 
+    if clients_arg.lower() == 'all':  # If the user types "all" it returns the full list of IP addresses.
+        return CLIENT_IPS # Return all client IP addresses stored in the CLIENT_IPS list.
+    return [ip.strip() for ip in clients_arg.split(',')] # this splits the addresses into individual IPs, removes any spaces, and returns them as a list.
